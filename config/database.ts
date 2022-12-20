187dcfd193b2c2b506f02cfac9a3c9afeaf24040
ericktateishi <ericktateishi@gmail.com>
2022-12-20 16:05:39 -0300

    prepare for deployment

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'mzjcbhknlbitvbj1'),
-      user: env('DATABASE_USERNAME', 'dsbud702369ug96m'),
-      password: env('DATABASE_PASSWORD', 'p001f6j5g8swjkn6'),
+      database: env('DATABASE_NAME', 'desgovernando'),
+      user: env('DATABASE_USERNAME', 'admin'),
+      password: env('DATABASE_PASSWORD', '12345'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
